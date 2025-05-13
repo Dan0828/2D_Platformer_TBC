bb3df97905f490d4cdd70c436a4f0f3e56bb9263
Dan0828 <alla7285@adsb.ca>
2025-05-13 09:28:33 -0400

    edit

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class SquareScript : MonoBehaviour
 {
-    public Rigidbody2D rigidBody;
+    private Rigidbody2D rigidBody;
+    public float moveSpeed = 12;
+    private float moveInput;
 
     void Start()
     {
-        
+        rigidBody = GetComponent<RigidBody2D>();
     }
 
     // Update is called once per frame
@@ -18,6 +20,9 @@ public class SquareScript : MonoBehaviour
         {
             rigidBody.velocity = Vector2.up * 18;
         }
+
+        moveInput = moveInput.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(moveInput * moveSpeed, rigidBody.velocity.y);
        
     }
 }
